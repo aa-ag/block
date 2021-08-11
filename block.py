b34d781121c780b9b8745548e84f5d084db4328d
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-10 20:41:10 -0500

    added three main global variables, main functiona and driver code

--- block.py
+++ block.py
@@ -3,11 +3,20 @@ import time
 from datetime import datetime
 
 ############------------ GLOBAL VARIABLE(S) ------------############
+### host
+host_path = ''
+
+### local host's IP
+redirect = ''
+
+### websites to block
+list_of_websites_to_block = list()
 
 
 ############------------ FUNCTION(S) ------------############
-
+def block_websites_from_list():
+    pass
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    pass
+    block_websites_from_list()
